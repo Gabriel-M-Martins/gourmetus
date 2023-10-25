3b8acb1634e5cdfa4d35ddc794c9151c61233f70
Lucas Cunha <lucascunha@10-41-48-22.0200.epoa.br.iosda.org>
2023-10-25 11:27:17 -0700

    feat: fetch datasource complete

--- Gourmetus/Gourmetus/Cookbook/CookBookModel.swift
+++ Gourmetus/Gourmetus/Cookbook/CookBookModel.swift
@@ -10,6 +10,13 @@ import Foundation
 struct CookBookModel {
     var favorites: [RecipeModel]
     var latest: [RecipeModel]
+    private var latestSize = 10
+    
+    init(favorites: [RecipeModel], latest: [RecipeModel], latestSize: Int = 10) {
+        self.favorites = favorites
+        self.latest = latest
+        self.latestSize = latestSize
+    }
     
     mutating func addFavorite(recipe:RecipeModel){
         favorites.append(recipe)
@@ -21,7 +28,25 @@ struct CookBookModel {
                 favorites.remove(at: i)
             }
         }
+        
     }
+    
+    mutating func addLatest(recipe:RecipeModel){
+        if(latest.count == latestSize){
+            latest.remove(at: latestSize)
+        }
+            latest.append(recipe)
+//        latest.sort(by: $0.date.compare($1.date) == orderedAscending)
+    }
+    
     
 }
 
+//        var f = Date.now
+//        var b = Date.now
+//        switch b.compare(f){
+//        case .orderedAscending:
+//            break
+//        }
+        
+//        latest.sort(by: $0.date.compare($1.date) == orderedAscending)
